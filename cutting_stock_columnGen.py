fc880c3dbc0271baa37668c38f84a283f94aa8cb
Roberto Carlos Hincapie Reyes <000010478@MED.UPB>
2025-03-08 21:00:49 -0500

    Se agregó ya funcionalmente el problema del Cutting Stock en una dimensión

--- cutting_stock_columnGen.py
+++ cutting_stock_columnGen.py
@@ -7,10 +7,11 @@ Created on Tue Feb 25 17:37:41 2025
 
 from pulp import *
 #Conunto de patrones existentes en el momento
-P=[[3,1,1]]
-d=[20,36,25]
-l=[2,5,9]
-L=20
+P=[[0,2,0,1],[6,1,1,0]]
+D=[40,30,35,8]
+h=[20,50,120,200]
+L=300
+
 def primal(P):
     x=[]
     fo=0
@@ -18,11 +19,11 @@ def primal(P):
         x.append(pulp.LpVariable('x_'+str(i),0,1000, cat='Integer'))
         fo+=x[-1]
     primal=LpProblem('Primal', LpMinimize)
-    for j in range(len(P[0])): #Iteramos sobre los productos que tenemos
+    for j in range(len(P[0])): #Iteramos sobre las piezas que necesitamos cortar
         tmp=0    
         for i in range(len(P)):
             tmp+=P[i][j]*x[i]
-        primal+=tmp>=d[j], 'Demanda del producto '+str(j)
+        primal+=tmp>=D[j], 'Demanda del producto '+str(j)
     primal+=fo, "Función objetivo"
     print(primal)
     primal.solve()
@@ -30,15 +31,13 @@ def primal(P):
     Z=primal.objective.value()
     return res, Z
 
-#res, Z=primal(P)
-#print('Primal:',res, Z)
 
 def dual(P):
     y=[]
     fo=0
-    for i in range(len(P[0])):
+    for i in range(len(D)):
         y.append(pulp.LpVariable('y_'+str(i),0,1000))
-        fo+=d[i]*y[-1]
+        fo+=D[i]*y[-1]
     dual=LpProblem('Dual', LpMaximize)
     for i in range(len(P)): #Iteramos sobre las variables de decisión actualmente disponibles
         tmp=0    
@@ -52,8 +51,6 @@ def dual(P):
     Z=dual.objective.value()
     return res, Z
 
-#resd, Zd = dual(P)
-#print('Dual:',resd, Zd)
 
 def secundario(y,l,L):
     u=[]
@@ -63,8 +60,8 @@ def secundario(y,l,L):
         fo-=y[i]*u[-1]
     secondary=LpProblem('Secondary', LpMinimize)
     tmp=0
-    for j in range(len(l)): #Iteramos sobre los productos que tenemos
-        tmp+=l[j]*u[j]
+    for j in range(len(h)): #Iteramos sobre los productos que tenemos
+        tmp+=h[j]*u[j]
     secondary+=tmp<=L, 'Longitud máxima de la pieza'
     secondary+=fo, "Función objetivo"
     print(secondary)
@@ -76,53 +73,14 @@ def secundario(y,l,L):
 #print('Secundario: ', resS)
 
 val=[]
+vZs=[]
 for i in range(10):
     res,Z=primal(P)
     val.append(Z)
     resd,Zd=dual(P)
-    resS, Sz, secondary=secundario(resd, l, L)
-    if(resS in P):
+    resS, Zs, secondary=secundario(resd, h, L)
+    vZs.append(Zs)
+    if(Zs>-1e-6):
         break
     P.append(resS)
 
-
-"""
-primal+=4*x1>=25, 'Producto 1'
-primal+=2*x2>=20, 'Producto 2'
-primal+=2*x3>=15, 'Producto 3'
-primal+=x1+x2+x3, 'Función objetivo'
-print('Problema primal')
-primal.solve()
-for v in primal.variables():
-   if(v.varValue>0):
-    print(v.name, "=", v.varValue)
-    
-y1=pulp.LpVariable('y1',0,1000)
-y2=pulp.LpVariable('y2',0,1000)
-y3=pulp.LpVariable('y3',0,1000)
-dual=LpProblem('Dual', LpMaximize)
-dual+=4*y1<=1, 'Rest 1'
-dual+=2*y2<=1, 'Rest 2'
-dual+=2*y3<=1, 'Rest 3'
-dual+=25*y1+20*y2+15*y3, 'Función objetivo'
-print('\n\nProblema dual')
-dual.solve()
-for v in dual.variables():
-   if(v.varValue>0):
-    print(v.name, "=", v.varValue)    
-    
-
-#Problema secundario
-u1=pulp.LpVariable('u1',0,1000, cat='Integer')
-u2=pulp.LpVariable('u2',0,1000, cat='Integer')
-u3=pulp.LpVariable('u3',0,1000, cat='Integer')
-second=LpProblem('Dual', LpMinimize)
-second+=5*u1+7*u2+9*u3<=20, 'Rest 1'
-second+=1-y1.value()*u1-y2.value()*u2-y3.value()*u3, 'Función objetivo'
-print('\n\nProblema secundario')
-second.solve()
-for v in second.variables():
-   if(v.varValue>0):
-    print(v.name, "=", v.varValue)    
-    
-"""
